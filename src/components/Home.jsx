90955c93f410098406a4ba4361ac087bf9fd2827
Femi Gabriel <timfemi@gmail.com>
2023-08-02 03:08:52 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductItem from "./ProductItem";
+
+const Home = () => {
+  return (
+    <div>
+      <ProductItem />
+    </div>
+  );
+};
+
+export default Home;
